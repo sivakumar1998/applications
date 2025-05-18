c1974d14e108e2f9f7efd521b68db7779d4f3896
Burra Siva Kumar <40349298+sivakumar1998@users.noreply.github.com>
2025-05-18 20:41:40 +0530

    added login functionality

--- login/login/urls.py
+++ login/login/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 
-from django.urls import path
+from django.urls import path,include
 from signin import views
+from users import views as users_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
+    path('',include('users.urls')),
+    path('signin/',include('signin.urls'))
 ]
